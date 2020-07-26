ba39e9a3bf1d106c3da096926f1617c699d30b78
LesMiserables1 <liemandre20@gmail.com>
2020-07-26 18:42:30 +0700

    change to puppeter

--- backend/index.js
+++ backend/index.js
@@ -10,19 +10,22 @@ const puppeteer = require('puppeteer');
 let app = express()
 app.use(bodyParser.json())
 
-app.get('/', async (req, res) => {
-  let url = 'https://boston.craigslist.org/search/off?query=wanted&availabilityMode=0'
-  // url = 'https://boston.craigslist.org/gbs/off/d/boston-modeling-beauty-spa-office-space/7157832724.html'
-
-  scraping3(url)
-  res.send('hellow')
-})
-
-app.post('/scrape',(req,res)=>{
+app.post('/scrape', async (req, res) => {
   let body = req.body
-  let url = `https://${body.location}.craigslist.org/search/off?query=${body.query}`
-  scraping1(url)
-  res.send(url)
+  let page_num = 0;
+  let status = true
+  while (status) {
+    let url = `https://${body.location}.craigslist.org/search/off?query=${body.query}&availabilityMode=0&s=${page_num}`
+    let html_page = await rp(url)
+    const $ = cheerio.load(html_page)
+    let page_count = $('span[class="button pagenum"]').first().text()
+    if (page_count == 'no results') {
+      status = false
+    }else{
+      page_num += 120
+    }
+  }
+  res.send("test")
 })
 
 let scraping1 = async (url) => {
@@ -55,33 +58,17 @@ let scraping1 = async (url) => {
   }
 }
 
-let scraping2 = async (url_list) => {
-  let email_list = []
-  let post_list = []
-  await url_list.forEach(async url => {
-    try {
-      const { Builder, By, Key, until } = require('selenium-webdriver');
-      let option = new Options()
-      let driver = await new Builder().forBrowser('firefox').setFirefoxOptions(option.headless()).build();
-      await driver.get(url)
-      var btn = await driver.findElement(By.className('reply-button js-only')).click()
-      await new Promise(r => setTimeout(r, 5000))
-      let email = await driver.findElement(By.xpath('/html/body/section/section/header/div[2]/div/div[1]/aside/ul/li[1]/p/a'))
-      let post_id = await driver.findElement(By.xpath('/html/body/section/section/section/div[2]/p[1]'))
-      email_list.push(email.getText())
-      post_list.push(post_id.getText())
-    } catch (err) {
-      console.log(err)
-    }
-  });
-  console.log(post_list)
-  return [email_list,post_list]
-}
-
-let scraping3 = async (url)=>{
-  const browser = await puppeteer.launch({headless: false})
+let scraping2 = async (url) => {
+  const browser = await puppeteer.launch()
   let page = await browser.newPage()
-  await page.goto('https://www.google.com')
+  await page.goto(url,{ waitUntil: 'networkidle0' })
+  let data = await page.evaluate(async()=>{
+    await document.querySelector('button[class="reply-button js-only"]').click()
+    await new Promise(r => setTimeout(r, 5000))
+    document.querySelector('div[class="print-information print-qrcode-container"').remove()
+    return [document.querySelector('a[class="mailapp"]').innerHTML,document.querySelector('p[class="postinginfo"').innerHTML,document.querySelector('section[id=postingbody').innerHTML]
+  })
+  return data
 }
 
 app.listen(3000)
