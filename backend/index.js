b4e18bcc2572b2ca14079a622b1c394ff05651a8
LesMiserables1 <liemandre20@gmail.com>
2020-07-28 13:57:41 +0700

    completed the MVP

--- backend/index.js
+++ backend/index.js
@@ -6,25 +6,38 @@ const puppeteer = require('puppeteer');
 
 let app = express()
 let url_list = [];
-let headline_list = []
-let area_list = []
 let data_list = []
 let status_scrape = 0
+
 app.use(bodyParser.json())
 
-app.post('/status',(req,res)=>{
-  if(status_scrape){
-    res.send(data_list)
-  }else
-  res.send(data_list)
+app.post('/content', (req, res) => {
+  let resp
+  if(!status_scrape){
+     resp = {status : status_scrape}
+  }else{
+    resp = 
+    {
+      'status': status_scrape,
+      'data': data_list 
+    }
+  }
+  res.send(resp)
 })
-app.post('/scrape', async (req, res) => {
+
+app.post('/scrape', (req, res) => {
   let body = req.body
-  let page_num = 0;
-  let status = true
   status_scrape = 0
   data_list = []
   url_list = []
+  filter(body).then(() => {
+    scraping2(url_list)
+  })
+  res.send({message:'success'})
+})
+let filter = async (body) => {
+  let status = true
+  let page_num = 0;
   while (status) {
     let url = `https://${body.location}.craigslist.org/search/off?query=${body.query}&availabilityMode=0&s=${page_num}`
     let html_page = await rp(url)
@@ -32,59 +45,73 @@ app.post('/scrape', async (req, res) => {
     let page_count = $('span[class="button pagenum"]').first().text()
     if (page_count == 'no results') {
       status = false
-    }else{
+    } else {
       scraping1(url)
       page_num += 120
     }
   }
-  scraping2(url_list)
-  res.send('job is on progress')
-})
+}
 
 let scraping1 = async (url) => {
-  
   try {
     let html = await rp(url)
     const $ = cheerio.load(html)
-
     $('a[class="result-title hdrlnk"]').each(function (index, element) {
       let el = $(element)
       url_list.push(el.attr('href'));
-      headline_list.push(el.text())
     });
-
-    $('span[class="result-hood"]').each(function (index, element) {
-      let el = $(element)
-      area_list.push(el.text())
-    })
-    $('span[class="nearby"]').each(function (index, element) {
-      let el = $(element)
-      area_list.push(el.text())
-    })
-    // console.dir(url_list);
-    // console.dir(headline_list)
-    // console.dir(area_list)
-    // let data = await scraping2(url_list)
-    // console.log(data)
-    // return data
   } catch (error) {
     console.log(error)
   }
 }
 
 let scraping2 = async (urls) => {
-  const browser = await puppeteer.launch({headless:false})
+  const browser = await puppeteer.launch({ headless: false })
   let page = await browser.newPage()
-  console.log(urls)
-  for (let i = 0; i < urls.length; ++i){
-    await page.goto(urls[i],{ waitUntil: 'networkidle0' })
-    let data = await page.evaluate(async()=>{
-      await document.querySelector('button[class="reply-button js-only"]').click()
-      await new Promise(r => setTimeout(r, 10000))
-      document.querySelector('div[class="print-information print-qrcode-container"').remove()
-      return [document.querySelector('a[class="mailapp"]').innerHTML,document.querySelector('p[class="postinginfo"').innerHTML,document.querySelector('section[id=postingbody').innerHTML]
-    })
-    data_list.push(data)
+  for (let i = 0; i < urls.length; ++i) {
+    try {
+      await page.goto(urls[i], { waitUntil: 'networkidle0' })
+      let data = await page.evaluate(async () => {
+        let list = []
+        await document.querySelector('button[class="reply-button js-only"]').click()
+        await new Promise(r => setTimeout(r, 8000))
+        document.querySelector('div[class="print-information print-qrcode-container"').remove()
+        try {
+          let email = { email: document.querySelector('a[class="mailapp"]').innerHTML }
+          list.push(email)
+        } catch (error) {
+          list.push({ email: "" })
+        }
+        try {
+          let headlines = { title: document.querySelector('span[class=postingtitletext]').textContent }
+          list.push(headlines)
+        } catch (error) {
+          list.push({ title: "" })
+        }
+        try {
+          let area = { address: document.querySelector('div[class=mapaddress]').innerHTML }
+          list.push(area)
+        } catch (error) {
+          list.push({ address: "" })
+        }
+        try {
+          let desc = { desc: document.querySelector('p[class="postinginfo"').innerHTML }
+          list.push(desc)
+        } catch (error) {
+          list.push({ desc: "" })
+        }
+        try {
+          let post_id = { post_id: document.querySelector('section[id=postingbody').innerHTML }
+          list.push(post_id)
+        } catch (error) {
+          list.push({ post_id: "" })
+        }
+        return list
+      })
+      data_list.push(data)
+    } catch (error) {
+      continue
+    }
   }
   status_scrape = 1
   return data_list
